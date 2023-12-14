2df20c69a3aa9bf6ffd4587a64e0e85ca66016d1
Cole Littlechilds <clittlechilds1@nait.ca>
2023-12-14 16:49:45 +0000

    Dec 14, 2023, 9:49 AM

--- tests/about.spec.js
+++ tests/about.spec.js
@@ -1,7 +1,13 @@
 import {describe, it, expect} from 'vitest';
+import {add} from '../src/about.js';
 
 describe('About this test suite', () => {
     it('should work', () => {
         expect(true).toBe(true);
+    })
+
+    it('should calculate 1 + 1', () => {
+        const actual = add(1,2);
+        const expected = 3;
     })
 })
